9a59412bfa979ca740ce729ffdab372bc52e75f5
Chris Sevilleja <chris@scotch.io>
2013-11-11 10:39:53 -0800

    adding views and handling angular routing

--- public/core.js
+++ public/core.js
@@ -1,5 +1,50 @@
 var scotchTodo = angular.module('scotchTodo', []);
 
+// adding routing for single page app (pull page from public/pages based on url)
+scotchTodo.config(function($routeProvider) {
+	$routeProvider
+
+		// home page, not logged in, ask people to login
+		.when('/', {
+			templateUrl : 'pages/home.html',
+			controller 	: 'homeController'
+		})
+
+		// signup page
+		.when('/signup', {
+			templateUrl : 'pages/signup.html',
+			controller 	: 'signupController'
+		})
+
+		// login page
+		.when('/login', {
+			templateUrl : 'pages/login.html',
+			controller 	: 'loginController'
+		})
+
+		// after they are signed in, show todos (this is the page from the first tutorial)
+		.when('/todos', {
+			templateUrl : 'pages/todos.html',
+			controller 	: 'mainController'
+		})
+});
+
+// controller for the home page ('/') (will show if not logged in)
+function homeController($scope, $http) {
+
+}
+
+// controller for the local signup page ('/signup')
+function signupController($scope, $http) {
+
+}
+
+// controller for the local login page ('/login')
+function loginController($scope, $http) {
+
+}
+
+// controller for the todos page ('/todos') (after they are signed in)
 function mainController($scope, $http) {
 	$scope.formData = {};
 
